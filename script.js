23159ce0977294e47075840abf61eb9276ac4499
LucasKleaL <lucaskleal222@outlook.com>
2020-09-21 17:55:08 -0300

    2 commit
    Adicionado nav bar no topo da tela.
    Adicionado menu lateral recolhível.

--- script.js
+++ script.js
@@ -9,8 +9,28 @@ $("#botaoConfirmar").click(function(){
     
     var wordNumber = texto.length;
     $("#visorContador").val(wordNumber)
-    alert(wordNumber)
+    alert(texto)
 });
 
+$("#botaoMenuLateral").click(function(){
+
+    var menuAtivado = parseInt(1);
+    var menuDesativado = parseInt(0);
+
+    if(menuAtivado === 1){
+        menuAtivado = 0;
+        menuDesativado = 1;
+
+        $("#menuLateral").removeClass("menu-lateral").addClass("menu-lateral-desativado")
+
+    }
+    else if(menuDesativado === 1){
+        menuAtivado = 1;
+        menuDesativado = 0;
+
+        $("#menuLateral").removeClass("menu-lateral-desativado").addClass("menu-lateral-ativado")
+    }
+
+});
 
 });
